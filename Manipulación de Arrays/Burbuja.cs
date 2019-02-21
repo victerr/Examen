97f2841fce6306cc2f73aa05a0fa817d84b21c41
victerr <44600180+victerr@users.noreply.github.com>
2019-02-21 18:34:52 +0100

    Update Burbuja.cs

--- Manipulación de Arrays/Burbuja.cs
+++ Manipulación de Arrays/Burbuja.cs
@@ -4,7 +4,9 @@ for (int i=0;i<cantidad-1;i++)
 	{
 		if (peliculas[i].titulo.CompareTo(peliculas[j].titulo)>0)
 		{
-		
+			peliculaAux.titulo=peliculas[i].titulo;
+			peliculas[i].titulo=peliculas[j].titulo;
+			peliculas[j].titulo=peliculaAux.titulo;
 		}
 							
 	}
